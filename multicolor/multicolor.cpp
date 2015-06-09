b9a44a259a860b947037aba042777281b657e26c
catbaron- <catbaron@live.cn>
2015-06-09 19:46:00 +0900

    correct the position

--- multicolor/multicolor.cpp
+++ multicolor/multicolor.cpp
@@ -57,7 +57,7 @@ void display(){
     gluQuadricDrawStyle(quadricObj, GLU_FLAT);
     float cy_hight = 0.5;
     //gltDrawUnitAxes();
-    glTranslatef(0.0, 0.0, -1.8);
+    glTranslatef(0.0, 0.0, -2);
 
     glPushMatrix();
     glRotatef(90, 1, 0, 0);
