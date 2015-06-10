f26b56c490f222f9eb1c5c7f13ba4b6bd27c4f15
catbaron- <catbaron@live.cn>
2015-06-10 16:40:17 +0900

    change glut to glfw

--- multicolor/multicolor.cpp
+++ multicolor/multicolor.cpp
@@ -1,53 +1,70 @@
+#include <GLFW/glfw3.h>
 #include <stdlib.h>
+#include <iostream>
 #include <GL/glut.h>
 #include <GL/gl.h>
-void windowInit();
+
 void display();
-void keybd(unsigned char key, int x, int y);
+static void keybd(GLFWwindow* window, int key, int scancode, int action,int mods);
 int main(int argc, char** argv)
 {
-    glutInit(&argc, argv);
-    //init window
-    windowInit();
-    //enable depth and cull_face
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-    glFrontFace(GL_CCW);
+    int width = 500, height = 600;
+    float aspect = (float)width / (float) height;
 
-    //enable light
-    GLfloat whiteLight[] = {0.05f, 0.05f, 0.05f, 1.0f};
-    GLfloat sourceLight[] = {0.25f, 0.25f, 0.25f, 1.0f};
-    GLfloat lightPos[] = {-5.0f, 15.0f, 5.0f, 1.0f};
-    glEnable(GL_LIGHTING);
-    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, whiteLight);
-    glLightfv(GL_LIGHT0, GL_AMBIENT_AND_DIFFUSE, sourceLight);
-    glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
-    glEnable(GL_LIGHT0); //开启颜色追踪 
-    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
-    glEnable(GL_COLOR_MATERIAL);
-    glutKeyboardFunc(keybd);
-    glutDisplayFunc(display);
-    glutMainLoop();
+    //init window
+    if(!glfwInit())
+        exit(EXIT_FAILURE);
+    //create window
+    GLFWwindow* window ;
+    window = glfwCreateWindow(width, height, "cylinder", NULL, NULL);
+    if(!window)
+    {
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, keybd);
+    glfwSwapBuffers(window);
+    glfwSwapInterval(1);
+
+    //Main Window Loop
+    while(!glfwWindowShouldClose(window))
+    {
+        glClearColor(1.0,1.0,1.0,1.0);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        //glOrtho(-1, 1.0, -1.0, 1.0, -1.0, 1.0);
+        gluPerspective(30.0,aspect,1.0,50.0);
+        glMatrixMode(GL_MODELVIEW);
+        //enable depth and cull_face
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
+        glFrontFace(GL_CCW);
+
+        //enable light
+        GLfloat whiteLight[] = {0.05f, 0.05f, 0.05f, 1.0f};
+        GLfloat sourceLight[] = {0.25f, 0.25f, 0.25f, 1.0f};
+        GLfloat lightPos[] = {-5.0f, 15.0f, 5.0f, 1.0f};
+        glEnable(GL_LIGHTING);
+        glLightModelfv(GL_LIGHT_MODEL_AMBIENT, whiteLight);
+        glLightfv(GL_LIGHT0, GL_AMBIENT_AND_DIFFUSE, sourceLight);
+        glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+        glEnable(GL_LIGHT0); //开启颜色追踪 
+        glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+        glEnable(GL_COLOR_MATERIAL);
+
+
+        //glViewport(0,0,width,height);
+        display();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    
     return(0);
 }
-void windowInit(){
-    int width = 500, height = 500;
-    float aspect = (float)width / (float) height;
-    glutInitWindowPosition(200,500);
-    glutInitWindowSize(width, height);
-    glutInitDisplayMode(GLUT_RGBA|GLUT_DEPTH);
-    glutCreateWindow("cylinder");
-    glClearColor(1.0,1.0,1.0,1.0);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    //glOrtho(-1, 1.0, -1.0, 1.0, -1.0, 1.0);
-    gluPerspective(30.0,aspect,1.0,50.0);
-    //glMatrixMode(GL_MODELVIEW);
-}
-
-void keybd(unsigned char key, int x, int y){
+static void keybd(GLFWwindow* window, int key, int scancode, int action,int mods){
     exit(0);
 }
 
@@ -57,7 +74,9 @@ void display(){
     gluQuadricDrawStyle(quadricObj, GLU_FLAT);
     float cy_hight = 0.5;
     //gltDrawUnitAxes();
-    glTranslatef(0.0, 0.0, -2);
+
+    glPushMatrix();
+    glTranslatef(0.0, 0.0, -3.5);
 
     glPushMatrix();
     glRotatef(90, 1, 0, 0);
@@ -73,7 +92,7 @@ void display(){
     glTranslatef(0.0, 0.0, -2*cy_hight);
     glColor3f(0.3, 0.2, 0.7);
     gluCylinder(quadricObj, 0.2, 0.2, cy_hight, 50, 13);
-
+    glPopMatrix();
     glPopMatrix();
     glFlush();
 }
